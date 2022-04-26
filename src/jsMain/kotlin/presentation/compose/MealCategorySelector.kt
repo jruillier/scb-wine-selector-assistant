1c5f2fd9979a37a344a3f1aa2ad8f241c9c78390
Jérôme Ruillier <jerome.ruillier@aronnax.eu>
2022-04-26 18:24:27 -0400

    Deletes unused Composable.

--- src/jsMain/kotlin/presentation/compose/MealCategorySelector.kt
+++
@@ -1,9 +0,0 @@
-package presentation.compose
-
-import androidx.compose.runtime.Composable
-import org.jetbrains.compose.web.dom.Text
-
-@Composable
-fun MealCategorySelector() {
-    Text("Meal Category")
-}
