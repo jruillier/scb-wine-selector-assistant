f8422bc7dc498222ec624cdc69f2c203c09f8d39
Jérôme Ruillier <jerome.ruillier@aronnax.eu>
2022-05-05 12:26:08 -0400

    Adds a background on button title when a background image is used.

--- src/jsMain/kotlin/presentation/jbCompose/SelectorButton.kt
+++ src/jsMain/kotlin/presentation/jbCompose/SelectorButton.kt
@@ -3,6 +3,7 @@ package presentation.jbCompose
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.Button
+import org.jetbrains.compose.web.dom.Span
 import org.jetbrains.compose.web.dom.Text
 
 @Composable
@@ -24,6 +25,15 @@ fun SelectorButton(text: String, imageSrc: String?, onClick: () -> Unit) {
             onClick.invoke()
         }
     }) {
-        Text(text)
+        Span({
+            style {
+                if (imageSrc != null) {
+                    padding(10.px)
+                    backgroundColor(rgba(0, 0, 0, 0.5))
+                }
+            }
+        }){
+            Text(text)
+        }
     }
 }
